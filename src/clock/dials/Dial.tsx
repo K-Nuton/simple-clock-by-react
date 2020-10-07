920fede569709cc195221dde0481c328c092ea15
Tjoakta04130 <8thonayah0@gmail.com>
2020-10-07 10:19:21 +0900

    文字盤に型情報を追加

--- src/clock/dials/Dial.tsx
+++ src/clock/dials/Dial.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import { ThemeContext } from '../ThemeContext';
 import './Dial.css'
@@ -8,18 +8,20 @@ type DialProps = {
   shape: string;
 };
 const Dial: React.FC<DialProps> = ({ children, shape }) => {
-  const theme = useContext(ThemeContext);
+  const theme: string = useContext(ThemeContext);
 
-  const decide_scaling = (): string => window.innerWidth < window.innerHeight ? "width-scaling" : "height-scaling";
+  const decide_scaling = (): string => 
+    window.innerWidth < window.innerHeight ? "width-scaling" : "height-scaling";
 
-  const [scaling, setScale] = useState(decide_scaling());
-  const resize = ():void => setScale(decide_scaling());
-
+  type scalingState = [string, React.Dispatch<React.SetStateAction<string>>];
+  const [scaling, setScale]: scalingState = useState(decide_scaling());
+  
+  const resize = (): void => setScale(decide_scaling());
 
   useEffect(() => {
     window.addEventListener("resize", resize);
 
-    return () => window.removeEventListener("resize", resize);
+    return (): void => window.removeEventListener("resize", resize);
   });
 
   return (
