bc4dacb74e16431e44a9df0f6897a6b057347ebc
Tjoakta04130 <8thonayah0@gmail.com>
2020-10-07 11:30:26 +0900

    時計本体に型情報を追加 reducerメソッドを最適化

--- src/clock/Clock.tsx
+++ src/clock/Clock.tsx
@@ -4,7 +4,7 @@ import { ThemeContext } from './ThemeContext';
 import Hands from './hands/Hands';
 import Marks from './marks/Marks';
 import Dial from './dials/Dial';
-import Selector from './selectors/Selector';
+import Selector, { ButtonProp } from './selectors/Selector';
 import './Clock.css';
 import './themes/neumorphism.css';
 import './themes/default.css';
@@ -13,43 +13,54 @@ import './themes/dotted.css';
 type themeType = {
   [key in Theme]: boolean
 };
-const themeSelectReducer = (clickState: themeType, innerText: Theme): themeType => {
-  const result: themeType = {
-    [Theme.DEFAULT]: false,
-    [Theme.NEU_MORPHISM]: false,
-    [Theme.DOTTED]: false
-  };
-  result[innerText] = true;
+const themeSelectReducer = (clickState: themeType, themeSelected: Theme): themeType => {
+  const result: themeType = { ...clickState };
+
+  for (const themeName of Object.keys(result))
+    result[themeName as Theme] = themeName === themeSelected;
+
   return result;
 };
 
 type shapeType = {
   [key in Shape]: boolean
 };
-const shapeSelectReducer = (clickState: shapeType, innerText: Shape): shapeType => {
-  const result: shapeType = {
-    [Shape.CIRCLE]: false,
-    [Shape.SQUARE]: false
-  }
-  result[innerText] = true;
+const shapeSelectReducer = (clickState: shapeType, shapeSelected: Shape): shapeType => {
+  const result: shapeType = { ...clickState };
+
+  for (const shapeName of Object.keys(result))
+    result[shapeName as Shape] = shapeName === shapeSelected
+
   return result;
 };
 
+type themeStateType = [string, React.Dispatch<React.SetStateAction<string>>];
+type themeDispatchType = [themeType, React.Dispatch<Theme>];
+
+type shapeStateType = [string, React.Dispatch<React.SetStateAction<string>>];
+type shapeDispatchType = [shapeType, React.Dispatch<Shape>];
+
 type ClockProp = {
 };
-const Clock: React.FC<ClockProp> = () => {
-  const [theme, setTheme] = useState(Theme.DEFAULT as string);
-  const [themeSelect, themeDispatch] = useReducer(themeSelectReducer, {
+const Clock: React.FC<ClockProp> = (): JSX.Element => {
+  const [theme, setTheme]: themeStateType = useState(Theme.DEFAULT as string);
+
+  const themeSelectInitialState: themeType = {
     [Theme.DEFAULT]: true,
     [Theme.NEU_MORPHISM]: false,
     [Theme.DOTTED]: false
-  });
+  };
+  const [themeSelect, themeDispatch]: themeDispatchType = 
+    useReducer(themeSelectReducer, themeSelectInitialState);
 
-  const [shape, setShape] = useState(Shape.CIRCLE as string);
-  const [shapeSelect, shapeDispatch] = useReducer(shapeSelectReducer, {
+  const [shape, setShape]: shapeStateType = useState(Shape.CIRCLE as string);
+
+  const shapeSelectInitialState: shapeType = {
     [Shape.CIRCLE]: true,
     [Shape.SQUARE]: false
-  });
+  };
+  const [shapeSelect, shapeDispatch]: shapeDispatchType = 
+    useReducer(shapeSelectReducer, shapeSelectInitialState);
 
   return (
     <div className={`overLay ${theme}`} >
@@ -61,20 +72,20 @@ const Clock: React.FC<ClockProp> = () => {
       </ThemeContext.Provider>
       <div className="button-wrapper">
         <Selector selection={
-          Object.entries(Theme).map(([_, themeName]) => ({
+          Object.values(Theme).map((themeName: Theme): ButtonProp => ({
             innerText: themeName,
             selected: themeSelect[themeName],
-            onClick: () => {
+            onClick: (): void => {
               setTheme(themeName);
               themeDispatch(themeName);
             }
           }))
         } />
         <Selector selection={
-          Object.entries(Shape).map(([_, shapeName]) => ({
+          Object.values(Shape).map((shapeName: Shape): ButtonProp => ({
             innerText: shapeName,
             selected: shapeSelect[shapeName],
-            onClick: () => {
+            onClick: (): void => {
               setShape(shapeName);
               shapeDispatch(shapeName);
             }
