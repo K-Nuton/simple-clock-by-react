412113f347f154c244c0d8d2373c81ab4ec4a398
Kusumoto Takanori <8thonayah0@gmail.com>
2020-10-07 10:33:37 +0900

    １２個のマークに型情報を追加

--- src/clock/marks/Marks.tsx
+++ src/clock/marks/Marks.tsx
@@ -8,7 +8,7 @@ type MarkProp = {
   degree: number;
   theme: string;
 };
-const Mark: React.FC<MarkProp> = ({ degree, theme }) => (
+const Mark: React.FC<MarkProp> = ({ degree, theme }): JSX.Element => (
   <div className={`mark ${theme}`} style={{ transform: `rotate(${degree}deg)` }}>
     <div></div>
   </div>
@@ -16,14 +16,14 @@ const Mark: React.FC<MarkProp> = ({ degree, theme }) => (
 
 type MarksProp = {
 };
-const Marks: React.FC<MarksProp> = () => {
-  const theme = useContext(ThemeContext);
+const Marks: React.FC<MarksProp> = (): JSX.Element => {
+  const theme: string = useContext(ThemeContext);
 
   return (
     <div className={`dial-overLay ${theme}`}>
       {[...Array(12)]
-        .map((_, index) => 30 * index)
-        .map((degree) => <Mark key={degree.toString()} degree={degree} theme={theme} />)}
+        .map((_: null, index: number): number => 30 * index)
+        .map((degree: number): JSX.Element => <Mark key={degree.toString()} degree={degree} theme={theme} />)}
     </div>
   );
 };
